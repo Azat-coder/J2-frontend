a7678b2226bc1dcef53caaa51d23668ecdd5cd7f
Azat Irgalin <aazatirgalin@gmail.com>
2025-04-10 22:47:51 +0500

    adds swapy to project

--- src/components/dashboard/StatsWidget.vue
+++ src/components/dashboard/StatsWidget.vue
@@ -1,24 +1,24 @@
 <template>
-    <div class="col-span-12 lg:col-span-6 xl:col-span-3">
-        <div class="card mb-0">
+    <div class="col-span-12 lg:col-span-6 xl:col-span-3" data-swapy-slot="5">
+        <div class="card mb-0" data-swapy-item="e">
             <div class="flex justify-between mb-4">
                 <div>
-                    <span class="block text-muted-color font-medium mb-4">Orders</span>
+                    <span class="block text-muted-color font-medium mb-4">Заказы</span>
                     <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">152</div>
                 </div>
                 <div class="flex items-center justify-center bg-blue-100 dark:bg-blue-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
                     <i class="pi pi-shopping-cart text-blue-500 !text-xl"></i>
                 </div>
             </div>
-            <span class="text-primary font-medium">24 new </span>
-            <span class="text-muted-color">since last visit</span>
+            <span class="text-primary font-medium">24 новых </span>
+            <span class="text-muted-color">с прошлого раза</span>
         </div>
     </div>
-    <div class="col-span-12 lg:col-span-6 xl:col-span-3">
-        <div class="card mb-0">
+    <div class="col-span-12 lg:col-span-6 xl:col-span-3" data-swapy-slot="6">
+        <div class="card mb-0" data-swapy-item="f">
             <div class="flex justify-between mb-4">
                 <div>
-                    <span class="block text-muted-color font-medium mb-4">Revenue</span>
+                    <span class="block text-muted-color font-medium mb-4">Выручка</span>
                     <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">$2.100</div>
                 </div>
                 <div class="flex items-center justify-center bg-orange-100 dark:bg-orange-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
@@ -26,14 +26,14 @@
                 </div>
             </div>
             <span class="text-primary font-medium">%52+ </span>
-            <span class="text-muted-color">since last week</span>
+            <span class="text-muted-color">с прошлой недели</span>
         </div>
     </div>
-    <div class="col-span-12 lg:col-span-6 xl:col-span-3">
-        <div class="card mb-0">
+    <div class="col-span-12 lg:col-span-6 xl:col-span-3" data-swapy-slot="7">
+        <div class="card mb-0" data-swapy-item="g">
             <div class="flex justify-between mb-4">
                 <div>
-                    <span class="block text-muted-color font-medium mb-4">Customers</span>
+                    <span class="block text-muted-color font-medium mb-4">Клиенты</span>
                     <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">28441</div>
                 </div>
                 <div class="flex items-center justify-center bg-cyan-100 dark:bg-cyan-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
@@ -41,22 +41,22 @@
                 </div>
             </div>
             <span class="text-primary font-medium">520 </span>
-            <span class="text-muted-color">newly registered</span>
+            <span class="text-muted-color">новых подписок</span>
         </div>
     </div>
-    <div class="col-span-12 lg:col-span-6 xl:col-span-3">
-        <div class="card mb-0">
+    <div class="col-span-12 lg:col-span-6 xl:col-span-3" data-swapy-slot="8">
+        <div class="card mb-0" data-swapy-item="h">
             <div class="flex justify-between mb-4">
                 <div>
-                    <span class="block text-muted-color font-medium mb-4">Comments</span>
-                    <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">152 Unread</div>
+                    <span class="block text-muted-color font-medium mb-4">Комментарии</span>
+                    <div class="text-surface-900 dark:text-surface-0 font-medium text-xl">152 не прочитано</div>
                 </div>
                 <div class="flex items-center justify-center bg-purple-100 dark:bg-purple-400/10 rounded-border" style="width: 2.5rem; height: 2.5rem">
                     <i class="pi pi-comment text-purple-500 !text-xl"></i>
                 </div>
             </div>
             <span class="text-primary font-medium">85 </span>
-            <span class="text-muted-color">responded</span>
+            <span class="text-muted-color">ответов</span>
         </div>
     </div>
 </template>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,19 +4,42 @@ import NotificationsWidget from '@/components/dashboard/NotificationsWidget.vue'
 import RecentSalesWidget from '@/components/dashboard/RecentSalesWidget.vue';
 import RevenueStreamWidget from '@/components/dashboard/RevenueStreamWidget.vue';
 import StatsWidget from '@/components/dashboard/StatsWidget.vue';
+import { createSwapy } from 'swapy';
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const swapy = ref(null);
+const container = ref();
+
+onMounted(() => {
+    if (container.value) {
+        swapy.value = createSwapy(container.value);
+
+        swapy.value.onSwap((event) => {
+            console.log('swap', event);
+        });
+    }
+});
+
+onUnmounted(() => {
+    swapy.value?.destroy();
+    container.value = null;
+});
 </script>
 
 <template>
-    <div class="grid grid-cols-12 gap-8">
+    <div class="grid grid-cols-12 gap-8" ref="container">
         <StatsWidget />
-
-        <div class="col-span-12 xl:col-span-6">
-            <RecentSalesWidget />
-            <BestSellingWidget />
+        <div class="col-span-12 xl:col-span-6" data-swapy-slot="1">
+            <RecentSalesWidget data-swapy-item="a" />
         </div>
-        <div class="col-span-12 xl:col-span-6">
-            <RevenueStreamWidget />
-            <NotificationsWidget />
+        <div class="col-span-12 xl:col-span-6" data-swapy-slot="2">
+            <BestSellingWidget data-swapy-item="b" />
+        </div>
+        <div class="col-span-12 xl:col-span-6" data-swapy-slot="3">
+            <RevenueStreamWidget data-swapy-item="c" />
+        </div>
+        <div class="col-span-12 xl:col-span-6" data-swapy-slot="4">
+            <NotificationsWidget data-swapy-item="d" />
         </div>
     </div>
 </template>
