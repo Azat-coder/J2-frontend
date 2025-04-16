bd897fd9b1fdb3aa453d664f4f7bb0379cf7abf4
Azat Irgalin <aazatirgalin@gmail.com>
2025-04-16 22:11:57 +0500

    adds dashboard changes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,14 +4,6 @@ import { useDashboardSwapy } from '@/shared/composables/useDashboardSwapy';
 const { container, slottedItems } = useDashboardSwapy();
 </script>
 
-<!-- <template>
-    <div class="grid grid-cols-12 gap-8" ref="container">
-        <div v-for="item in dashboardItems" :key="item.id" :class="item.cols" :data-swapy-slot="item.slot">
-            <component :is="item.component" :data-swapy-item="item.id" />
-        </div>
-    </div>
-</template> -->
-
 <template>
     <div class="grid grid-cols-12 gap-8" ref="container">
         <div 
