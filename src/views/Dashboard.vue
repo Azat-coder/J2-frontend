c19e2d2da87b6991b2450498b466fd213c14f90c
Azat Irgalin <aazatirgalin@gmail.com>
2025-04-27 13:49:44 +0500

    provides small refactoring

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,49 +1,48 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
 import { useDashboardStore } from '@/shared/stores/useDashboardStore'
 
 const dashboard = useDashboardStore()
 
 function handleMove(event) {
-  console.log('[handleMove]', event)
-  dashboard.saveDashboardState()
+    console.log('[handleMove]', event)
+    dashboard.saveDashboardState()
 }
 
 function handleResize(event) {
-  console.log('[handleResize]', event)
-  dashboard.saveDashboardState()
+    console.log('[handleResize]', event)
+    dashboard.saveDashboardState()
 }
 
 function handleMoveEnd(newLayout) {
-  console.log('[handleMoveEnd]', newLayout)
-  dashboard.handleMoveEnd(newLayout)
+    console.log('[handleMoveEnd]', newLayout)
+    dashboard.handleMoveEnd(newLayout)
 }
 </script>
 
 <template>
-  <grid-layout
-    v-model:layout="dashboard.dashboardLayout"
-    :col-num="12"
-    :row-height="30"
-  >
-    <template #default="{ gridItemProps }">
-      <grid-item
-        v-for="item in dashboard.dashboardLayout"
-        v-bind="gridItemProps"
-        :id="item.id"
-        :key="item.id"
-        :x="item.x"
-        :y="item.y"
-        :w="item.w"
-        :h="item.h"
-        @noc-resize="handleResize"
-        @noc-move="handleMove"
-        @noc-move-end="handleMoveEnd"
-      >
-        <component
-          :is="dashboard.dashboardItems.find(w => w.id === item.id)?.component"
-        />
-      </grid-item>
-    </template>
-  </grid-layout>
+    <grid-layout
+        v-model:layout="dashboard.dashboardLayout"
+        :col-num="12"
+        :row-height="30"
+    >
+        <template #default="{ gridItemProps }">
+        <grid-item
+            v-for="item in dashboard.dashboardLayout"
+            v-bind="gridItemProps"
+            :id="item.id"
+            :key="item.id"
+            :x="item.x"
+            :y="item.y"
+            :w="item.w"
+            :h="item.h"
+            @noc-resize="handleResize"
+            @noc-move="handleMove"
+            @noc-move-end="handleMoveEnd"
+        >
+            <component
+            :is="dashboard.dashboardItems.find(w => w.id === item.id)?.component"
+            />
+        </grid-item>
+        </template>
+    </grid-layout>
 </template>
