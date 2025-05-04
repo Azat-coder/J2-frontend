32a39405854f47f5b7454e537c552bfd141cc58a
Azat Irgalin <aazatirgalin@gmail.com>
2025-05-04 21:51:22 +0500

    Updates

---
+++ assets/Empty-DZUy9um6.js
@@ -0,0 +1,1 @@
+import{ai as s,o,b as a,h as e}from"./index-D7rIgvhR.js";const c={},r={className:"card"};function n(l,t){return o(),a("div",r,t[0]||(t[0]=[e("div",{class:"font-semibold text-xl mb-4"},"Empty Page",-1),e("p",null,"Use this page to start from scratch and place your custom content.",-1)]))}const d=s(c,[["render",n]]);export{d as default};
