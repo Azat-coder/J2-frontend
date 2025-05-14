6cc966afe6972bc999bd4f5a73d6c1f43fa3d329
Azat Irgalin <aazatirgalin@gmail.com>
2025-05-14 23:01:41 +0500

    Updates

--- assets/WorldMap-ComTg3UQ.js
+++
@@ -1,1 +0,0 @@
-import{o as e,b as a,h as s,j as t,f as c,I as o}from"./index-9PW8RyKV.js";const l={class:"w-full h-full"},r={class:"card mb-0 w-full h-full"},u={__name:"WorldMap",setup(_){return(n,d)=>(e(),a("div",l,[s("div",r,[t(c(o))])]))}};export{u as default};
